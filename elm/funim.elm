51f7f267ab50c31b2938edbb731937e857aae62a
glendc <decauwsemaecker.glen@gmail.com>
2015-05-27 23:45:20 +0000

    grid

--- elm/funim.elm
+++ elm/funim.elm
@@ -1,7 +1,34 @@
 module Funim where
 
+import Color exposing (..)
+import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
-
-main : Element
+import Window
+  
+main : Signal Element
 main =
-  show "Hello, World!"
+  Signal.map render Window.dimensions
+
+renderGridLine : (Int, Int, Int, Int) -> Form
+renderGridLine (ax, ay, bx, by) =
+  traced (dashed grey) (path ([(toFloat ax, toFloat ay), (toFloat bx, toFloat by)]))
+
+renderGridLines : Int -> Int -> ((Int,  Int) -> Form) -> List Form
+renderGridLines (size, i, renderLine) =
+  if i == 0 then [] else
+    let i = i - 1
+        pos = i * size 
+    in (renderLine pos size) :: renderGridLines size i renderLine
+
+renderGrid : (Int, Int, Int) -> Element
+renderGrid (size, gx, gy) =
+  let renderHor = \pos size -> (renderGridLine pos 0 pos size)
+      renderVer = \pos size -> (renderGridLine 0 pos size pos)
+      grid = renderGridLines size gy renderHor ++
+        renderGridLines size gx renderVer
+  in
+    collage size size grid
+
+render : (Int, Int) -> Element
+render (x, y) =
+  renderGrid (x, 4, 4)
