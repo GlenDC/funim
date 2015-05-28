639e0be5b978210f7111ff8b8eabfe60f3524523
glendc <decauwsemaecker.glen@gmail.com>
2015-05-28 22:05:32 +0000

    padding

--- elm/funim.elm
+++ elm/funim.elm
@@ -11,7 +11,7 @@ main =
 
 renderGridLine : Int -> Int -> Int -> Int -> Form
 renderGridLine ax ay bx by =
-  traced (dashed red) (path ([(toFloat ax, toFloat ay), (toFloat bx, toFloat by)]))
+  traced (solid grey) (path ([(toFloat ax, toFloat ay), (toFloat bx, toFloat by)]))
 
 renderGridLines : Int -> Int -> Int -> (Int -> Int -> Form) -> List Form
 renderGridLines gridsz cellsz i renderLine =
@@ -20,15 +20,23 @@ renderGridLines gridsz cellsz i renderLine =
         pos = j * cellsz 
     in (renderLine pos gridsz) :: (renderGridLines gridsz cellsz j renderLine)
 
-renderGrid : Int -> Int -> Int -> Int -> Element
-renderGrid gridsz cellsz gx gy =
+renderGrid : Int -> Int -> Int -> Int -> Int -> Element
+renderGrid gridsz cellsz gx gy padding =
   let renderHor = \pos sz -> renderGridLine pos 0 pos sz 
       renderVer = \pos sz -> renderGridLine 0 pos sz pos
       grid = (renderGridLines gridsz cellsz gy renderHor) ++
         (renderGridLines gridsz cellsz gx renderVer)
+      collagesz = gridsz + (padding * 2) 
+      trans = negate ((toFloat gridsz) / 2.0)
   in
-    collage gridsz gridsz grid
+    grid
+      |> List.map (\x -> move (trans, trans) x)
+      |> collage collagesz collagesz
 
 render : (Int, Int) -> Element
 render (x, y) =
-  renderGrid 400 50 8 8
+  let padding = 50
+      sz = (min x y) - (padding*2)
+      cx = 25
+      ccx = round (toFloat sz / toFloat cx)
+  in renderGrid sz ccx (cx+1) (cx+1) 50
