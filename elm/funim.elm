98f135cf1549e7588c95f4e5ed250d796088ff17
glendc <decauwsemaecker.glen@gmail.com>
2015-05-28 22:17:23 +0000

    Fixed floating point errors

--- elm/funim.elm
+++ elm/funim.elm
@@ -9,25 +9,25 @@ main : Signal Element
 main =
   Signal.map render Window.dimensions
 
-renderGridLine : Int -> Int -> Int -> Int -> Form
+renderGridLine : Float -> Float -> Float -> Float -> Form
 renderGridLine ax ay bx by =
-  traced (solid grey) (path ([(toFloat ax, toFloat ay), (toFloat bx, toFloat by)]))
+  traced (solid grey) (path ([(ax, ay), (bx, by)]))
 
-renderGridLines : Int -> Int -> Int -> (Int -> Int -> Form) -> List Form
+renderGridLines : Float -> Float -> Int -> (Float -> Float -> Form) -> List Form
 renderGridLines gridsz cellsz i renderLine =
   if i == 0 then [] else
     let j = i - 1
         pos = j * cellsz 
     in (renderLine pos gridsz) :: (renderGridLines gridsz cellsz j renderLine)
 
-renderGrid : Int -> Int -> Int -> Int -> Int -> Element
+renderGrid : Float -> Float -> Int -> Int -> Int -> Element
 renderGrid gridsz cellsz gx gy padding =
-  let renderHor = \pos sz -> renderGridLine pos 0 pos sz 
-      renderVer = \pos sz -> renderGridLine 0 pos sz pos
+  let renderHor = \pos sz -> renderGridLine pos 0.0 pos sz 
+      renderVer = \pos sz -> renderGridLine 0.0 pos sz pos
       grid = (renderGridLines gridsz cellsz gy renderHor) ++
         (renderGridLines gridsz cellsz gx renderVer)
-      collagesz = gridsz + (padding * 2) 
-      trans = negate ((toFloat gridsz) / 2.0)
+      collagesz = (round gridsz) + (padding * 2)
+      trans = negate (gridsz / 2.0)
   in
     grid
       |> List.map (\x -> move (trans, trans) x)
@@ -36,7 +36,7 @@ renderGrid gridsz cellsz gx gy padding =
 render : (Int, Int) -> Element
 render (x, y) =
   let padding = 50
-      sz = (min x y) - (padding*2)
-      cx = 25
-      ccx = round (toFloat sz / toFloat cx)
+      sz = toFloat ((min x y) - (padding*2))
+      cx = 5
+      ccx = sz / cx
   in renderGrid sz ccx (cx+1) (cx+1) 50
