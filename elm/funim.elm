3ad8c9032c8426c8970ceba6fd6e19a8a9a753c0
glendc <decauwsemaecker.glen@gmail.com>
2015-05-31 21:09:03 +0000

    When finishing a level, the next static level will be loaded in an inf loop

--- elm/funim.elm
+++ elm/funim.elm
@@ -8,23 +8,7 @@ import Window
 import Mouse
 import Array exposing (Array)
 import Text
-import Debug
-
--- TODO:
----- Fix Positioning grid
------- Need Title + one line instruction [TOP]
------- Ned Control instructions [BOTTOM]
----- Replace lines by squares
------- this should be based on the state (on / off)
----- Add Mouse Click Signal (left)
------- check if on Square, if so change state
----- Game logic -> If complete light or dark -> Next Level
----- store game logic in browser coockie
----- add reset button
----- Add 5 Levels
----- Make Levels generate automatically based on
------- Steps Sequence (logic)
------- Rules (only generated solvable levels)
+import Maybe
 
 levelsDatabase = Array.fromList [
   [ False ], -- level 1, Nv. 1
@@ -37,20 +21,26 @@ levelsDatabase = Array.fromList [
     True, False, True, True, False,
     False, False, False, True, True ] ]
 
-getLevel : Int -> List Bool
+getLevel : Int -> (Int, List Bool)
 getLevel level =
   case (Array.get level levelsDatabase) of
-    Just list -> list 
-    Nothing -> []
+    Just list -> (level, list)
+    Nothing ->
+      if | level < 0 -> getLevel ((Array.length levelsDatabase) - 1)
+         | otherwise -> getLevel 0
 
 type alias State = { level : Int, count : Int, grid : List Bool }
 
-getInitialGameState : Bool -> State
-getInitialGameState checkCache =
-  let level = 2
-      grid = getLevel level
+getNewLevel : Int -> State
+getNewLevel level' =
+  let (level, grid) = getLevel level'
       count = grid |> List.length |> toFloat |> sqrt |> floor
   in { level = level, count = count, grid = grid }
+
+
+getInitialGameState : Bool -> State
+getInitialGameState checkCache =
+  getNewLevel 0
 
 lightQuery : Signal.Mailbox Int
 lightQuery = Signal.mailbox 0
@@ -94,6 +84,14 @@ renderText content x y sz =
     |> Text.typeface [ "Helvetica" ]
     |> Text.color darkGrey |> Text.height sz |> text)
 
+getLevelDesc : Int -> String
+getLevelDesc level =
+  case level of
+    0 -> "Toggle any light by clicking on it."
+    1 -> "Surrounding lights are effected to!"
+    2 -> "Can you still handle it?"
+    _ -> ""
+
 render : (Int, Int) -> State -> Element
 render (x, y) state =
   let smallest = toFloat (min x y)
@@ -111,7 +109,7 @@ render (x, y) state =
       toForm (collage contextsz contextsz
         ((renderText "Light || Dark"
             0 (halfsz - h1sz) h1sz) ::
-         (renderText "Toggle any light by clicking on it."
+         (renderText (getLevelDesc state.level)
             0 (negate (halfsz - (h2sz * 2))) h2sz) ::
          (renderGrid state.grid state.count cellsz offset []))
        |> container x y middle)]
@@ -138,7 +136,18 @@ updateGrid i target count lights aux =
       updateGrid (i + 1) target count tl
         ((updateLight i target count hd) :: aux)
 
+areWeFinished : Bool -> List Bool -> Bool
+areWeFinished isOn lights =
+  case lights of
+    [] -> True
+    hd::tl -> if hd == isOn then (areWeFinished isOn tl) else False
+
 update : Int -> State -> State
 update i state =
   let newGrid = updateGrid 0 i state.count state.grid []
-  in { state | grid <- newGrid }
+  in
+    if (areWeFinished
+          (Maybe.withDefault False (List.head newGrid))
+          (Maybe.withDefault [] (List.tail newGrid)))
+      then getNewLevel (state.level + 1)
+      else { state | grid <- newGrid }
